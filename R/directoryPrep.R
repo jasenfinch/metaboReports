198ebbba6f0371b9f456797bce933027fa4efdef
Jasen <jsf9@aber.ac.uk>
2017-11-07 16:14:49 +0000

    directory prep now accepts ReportParameters

--- R/directoryPrep.R
+++ R/directoryPrep.R
@@ -1,13 +1,13 @@
 #' @importFrom stringr str_c
 
-directoryPrep <- function(analysis,title,path = '.'){
+directoryPrep <- function(analysis,parameters){
+  title <- parameters@title
+  path <- parameters@path
+  
   reportPath <- str_c(path,title,sep = '/')
   
   dirCmd <- str_c('mkdir',reportPath,sep = ' ')
   system(dirCmd)
   
   save(analysis,file = str_c(reportPath,'reportData.RData',sep = '/'))
-  
-  reportCmd <- str_c('touch',str_c(reportPath,str_c(title,'.Rmd'),sep = '/'),sep = ' ')
-  system(reportCmd)
 }
