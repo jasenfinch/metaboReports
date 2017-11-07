afe7dee9af08356d4ae8053f78cef6304ebbbfe6
Jasen <jsf9@aber.ac.uk>
2017-11-07 18:16:12 +0000

    added QCsection

---
+++ R/QCsection.R
@@ -0,0 +1,29 @@
+
+QCsection <- function(analysis,parameters){
+  parameters <- parameters@QC
+  
+  section <- '
+## Quality Control
+
+  ```{r RSD,echo=FALSE}
+suppressMessages(analysis %>%
+  resultsAnalysis() %>%
+  plotRSD()
+  )
+```
+
+```{r PCA,echo=FALSE}
+analysis %>%
+  resultsAnalysis() %>%
+  plotPCA()
+```
+
+```{r LDA,echo=FALSE}
+analysis %>%
+  resultsAnalysis() %>%
+  plotLDA()
+```
+  '
+  
+  return(section)
+}
