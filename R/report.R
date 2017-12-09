3d4f482713259cc9451374647616c496c5a49c50
Jasen <jsf9@aber.ac.uk>
2017-12-09 20:27:27 +0000

    added report example

--- R/report.R
+++ R/report.R
@@ -1,12 +1,23 @@
+#' report
+#' @importFrom magrittr %>%
 #' @importFrom readr write_file
 #' @importFrom rmarkdown render
+#' @examples 
+#' data("exampleFIEworkflowResults")
+#' p <- reportParameters(analysis,'test')
+#' report(analysis,p)
 #' @export
 
 report <- function(analysis,parameters){
   
   directoryPrep(analysis,parameters)
   
-  report <- reportHeader(analysis,parameters)
+  report <- c(reportHeader(analysis,parameters),
+              QCsection(analysis,parameters),
+              classificationSection(analysis,parameters),
+              reportFooter()
+              ) %>%
+    str_c(collapse = '')
   
   reportFile <- str_c(parameters@path,parameters@title,parameters@title,sep = '/')
                       
