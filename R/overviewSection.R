222844b5e51f87e07f1f921dace968bdfaf1ef05
jasenfinch <jsf9@aber.ac.uk>
2018-01-25 12:44:10 +0000

    added overview section

---
+++ R/overviewSection.R
@@ -0,0 +1,42 @@
+#' @importFrom stringr str_c
+#' @importFrom binneRlyse info
+
+overViewSection <- function(analysis){
+  
+  overview <- '## Overview'
+  
+  if (class(analysis) == 'Workflow') {
+    overview <- str_c(overview,
+'
+      
+**Technique:** Workflow - ',analysis@logs,'
+
+'
+    )
+  }
+  
+  if (class(analysis) == 'Binalysis') {
+    overview <- str_c(overview,
+'
+      
+**Technique:** FIE-HRMS Spectral Binning
+
+**Date undertaken:** ',analysis@binLog,'
+
+**Number of samples:** ',nrow(info(analysis)),'
+      
+'
+    )
+  }
+  
+  if (class(analysis) == 'Analysis') {
+    overview <- str_c(overview,
+'
+      
+**Technique:** metabolyseR analysis
+
+'
+    )
+  }
+  return(overview)
+}
