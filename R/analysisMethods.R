6481a4f8dd3e298ac0d667400c29a43881cc83d7
jasenfinch <jsf9@aber.ac.uk>
2018-02-08 17:37:45 +0000

    lowered r version dependency to 3.4.0

--- R/analysisMethods.R
+++ R/analysisMethods.R
@@ -1,6 +1,15 @@
 
 analysisMethods <- function(method){
   method <- list(
+    parameters = list(function(analysis){
+      "
+### Parameters
+
+```{r analysisParamters,echo=FALSE}
+analysis@parameters
+```
+"
+    }),
     preTreatment = preTreatmentMethods(),
     classification = classificationMethods(),
     featureSelection = featureSelectionMethods(),
--- R/classifcationMethods.R
+++ R/classifcationMethods.R
@@ -1,4 +1,4 @@
 
 classificationMethods <- function(method){
-  
+
 }
