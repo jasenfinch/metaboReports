33ff667ee9841f1cf3fde00335d5c742c4d130bc
jasenfinch <jsf9@aber.ac.uk>
2018-07-23 00:09:05 +0100

    compressed example data and added documentation

---
+++ R/exampleFIEworkflowResults.R
@@ -0,0 +1,5 @@
+#' exampleFIEworkflowResults
+#' @description example FIE-HRMS workflow results from the example B. distachyon ecotype data in package \code{methaboData}.
+#' @format An S4 object of class `Workflow`
+
+'exampleFIEworkflowResults'
