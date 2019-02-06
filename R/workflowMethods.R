42cc6a3dccbbd5172c3a6008b004c724ed894aaa
jasenfinch <jsf9@aber.ac.uk>
2019-02-06 10:47:48 +0000

    added profiling methods for workflows

--- R/workflowMethods.R
+++ R/workflowMethods.R
@@ -2,7 +2,9 @@
 workflowMethods <- function(analysis){
   method <- list(
     FIE_HRMSfingerprinting = FIEfingerprintingMethods,
-    FIE_HRMSfingerprinting2 = FIEfingerprintingMethods
+    FIE_HRMSfingerprinting2 = FIEfingerprintingMethods,
+    NP_LC_HRMSprofiling = profilingMethods(),
+    RP_LC_HRMSprofiling = profilingMethods()
   )
   method <- method[[analysis@workflowParameters@workflow]]
   return(method)
