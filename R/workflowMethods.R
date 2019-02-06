8e768fe4e59166064caee6e8a6e58880a611bfd4
jasenfinch <jsf9@aber.ac.uk>
2019-02-06 11:59:48 +0000

    fix workflow report error. Changed version to 0.5.2

--- R/workflowMethods.R
+++ R/workflowMethods.R
@@ -3,8 +3,8 @@ workflowMethods <- function(analysis){
   method <- list(
     FIE_HRMSfingerprinting = FIEfingerprintingMethods,
     FIE_HRMSfingerprinting2 = FIEfingerprintingMethods,
-    NP_LC_HRMSprofiling = profilingMethods(),
-    RP_LC_HRMSprofiling = profilingMethods()
+    NP_LC_HRMSprofiling = profilingMethods,
+    RP_LC_HRMSprofiling = profilingMethods
   )
   method <- method[[analysis@workflowParameters@workflow]]
   return(method)
