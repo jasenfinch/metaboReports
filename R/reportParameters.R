330b4a565eb5498f1d28178aa1577275911ffe87
jasenfinch <jsf9@aber.ac.uk>
2019-08-13 14:33:40 +0100

    made session info section optional

--- R/reportParameters.R
+++ R/reportParameters.R
@@ -2,15 +2,19 @@
 #' @description Initiate report parameters. 
 #' @param title report title
 #' @param investigator investigator name
+#' @param sessionInfo TRUE/FALSE, should session info included?
+#' @param toc TRUE/FALSE, should a table of contents be included
 #' @param path report output path
 #' @importFrom methods new
 #' @export
 
-reportParameters <- function(title,investigator,path = '.') {
+reportParameters <- function(title, investigator, sessionInfo = TRUE, toc = TRUE, path = '.') {
   new('ReportParameters',
       path = path,
       title = title,
       investigator = investigator,
+      sessionInfo = sessionInfo,
+      toc = toc,
       date = Sys.Date(),
       output = 'html_document'
   )
