a48bc057021f60ccd90010e2db970c0c7696a627
jasenfinch <jsf9@aber.ac.uk>
2020-12-11 15:45:41 +0000

    Report chunks added to own file, exported, added documentation

--- R/render.R
+++ R/render.R
@@ -24,3 +24,42 @@ setMethod('renderReport',signature = 'Report',
               browseURL( glue("{path(x)}/{report_title}/{report_title}.html")) 
             }
           })
+
+#' @importFrom stringr str_replace_all
+#' @importFrom rstudioapi initializeProject isAvailable
+#' @importFrom readr write_file write_rds
+#' @importFrom purrr walk
+
+setMethod('reportDirectory',signature = 'Report',
+          function(x){
+            
+            message('Creating report directory')
+            
+            report_title <-  x %>%
+              title() %>%
+              str_replace_all(' ','_')
+            
+            report_path <- glue("{path(x)}/{report_title}")
+            
+            glue("{report_path}/data") %>%
+              dir.create(recursive = TRUE)
+            
+            message('Saving report data')
+            
+            if (isAvailable()) {
+              initializeProject(report_path) 
+            }
+            
+            report_rmd <- reportRMD(x)
+            
+            write_file(report_rmd,glue("{report_path}/{report_title}.Rmd"))
+            
+            report_data <- reportData(x)
+            
+            report_data %>%
+              names() %>%
+              walk(~{
+                file_name <- sanitiseArgumentNames(.x)
+                write_rds(report_data[[.x]],glue("{report_path}/data/{file_name$file_name}.rds"))
+              })
+          })
